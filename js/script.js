79b18c9be53cad931301dd9f0655d4dd81c0ed38
Dominika666 <zajacowna666@gmail.com>
2022-11-30 09:41:19 +0100

    Add changes

--- js/script.js
+++ js/script.js
@@ -2,22 +2,30 @@
 document.addEventListener('DOMContentLoaded', function(){
 
 const nav = document.querySelector('.navbar')
+const allNavLinks = document.querySelectorAll('.nav-link')
+const navList = document.querySelector('.navbar-collapse')
 
 
 function addShadow() {
     
     if(window.scrollY >= 100) {
-        nav.classList.add('shadow-bg')
+        nav.classList.add('shadow-bg');
+        
+
+
     } else {
         nav.classList.remove('shadow-bg')
     }
 }
 
 
+allNavLinks.forEach(link => link.addEventListener('click',()=> navList.classList.remove('show')))
 
 
 window.addEventListener('scroll', addShadow)
 
+
 })
+
 
 
